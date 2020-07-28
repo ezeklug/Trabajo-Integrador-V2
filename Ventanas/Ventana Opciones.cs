a2c1316fcb5ff216b915f9d9f4f044b389dfbfeb
victoriaponsiano <56657579+victoriaponsiano@users.noreply.github.com>
2020-07-28 16:43:28 -0300

    Solucionado ventana opciones

--- Ventanas/Ventana Opciones.cs
+++ Ventanas/Ventana Opciones.cs
@@ -23,37 +23,46 @@ namespace Trabajo_Integrador.Ventanas
 
         private void listaUsuarios_Click(object sender, EventArgs e)
         {
+            this.Hide();
             Ventana_Lista_Usuarios listaUsuarios = new Ventana_Lista_Usuarios(iNombreUsuario);
             listaUsuarios.ShowDialog();
+            this.Close();
         }
 
         private void verExamenes_Click(object sender, EventArgs e)
         {
+            this.Hide();
             Ventana_Lista_Examenes listaExamenes = new Ventana_Lista_Examenes(iNombreUsuario);
             listaExamenes.ShowDialog();
+            this.Close();
         }
 
       
 
         private void setAdministrador_Click(object sender, EventArgs e)
         {
+            this.Hide();
             Ventana_Set_Administrador setAdministrador = new Ventana_Set_Administrador();
             setAdministrador.ShowDialog();
-
+            this.Close();
 
 
         }
 
         private void verPreguntas_Click(object sender, EventArgs e)
         {
+            this.Hide();
             Ventana_Todas_las_Preguntas vPreguntas = new Ventana_Todas_las_Preguntas(iNombreUsuario);
             vPreguntas.ShowDialog();
+            this.Close();
         }
 
         private void modificarTiempos_Click(object sender, EventArgs e)
         {
+            this.Hide();
             Ventana_Modificar_Tiempo vTiempos = new Ventana_Modificar_Tiempo(iNombreUsuario);
             vTiempos.ShowDialog();
+            this.Close();
         }
 
        
@@ -69,8 +78,10 @@ namespace Trabajo_Integrador.Ventanas
         
         private void cargarPreguntas_Click_1(object sender, EventArgs e)
         {
+            this.Hide();
             Ventana_Cargar_Pregunta vCargarExamen = new Ventana_Cargar_Pregunta(iNombreUsuario);
             vCargarExamen.ShowDialog();
+            this.Close();
 
         }
     }
