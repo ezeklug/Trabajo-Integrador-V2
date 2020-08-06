181aaa6d9cdcc12e7404395b66313a9cce5cfa5f
leohoet98@gmail.com <leohoet98@gmail.com>
2020-08-06 20:22:24 -0300

    Modificado EF

---
+++ EntityFramework/RepositorioRespuesta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trabajo_Integrador.Dominio;
+
+namespace Trabajo_Integrador.EntityFramework
+{
+    public class RepositorioRespuesta : Repository<Respuesta, TrabajoDbContext>
+    {
+        public RepositorioRespuesta(TrabajoDbContext pContext) : base(pContext) { }
+    }
+}
