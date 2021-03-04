61c69630f1417e4a65714f7f7e0de670cc420663
Victoria <victoriaponsiano@gmail.com>
2021-03-04 15:58:51 -0300

    Mas testes de controlador preguntas

--- UnitTests/TestControladorPreguntas.cs
+++ UnitTests/TestControladorPreguntas.cs
@@ -21,12 +21,13 @@ namespace UnitTests
        [TestMethod]
         public void TestCargarPreguntasDeInternet()
         {
-            string pCantidad = "10";
+            string pCantidad = "20";
             string pConjunto = "OpentDb";
             string pCategoria = "Sports";
             string pDificultad = "easy";
 
             var count = ControladorPreguntas.ObtenerPreguntasDeInternet(pCantidad,pConjunto,pCategoria,pDificultad).ToList().Count;
+            Console.WriteLine(count);
             Assert.IsTrue(count >= 1);
 
 
@@ -39,6 +40,35 @@ namespace UnitTests
             int count = ControladorPreguntas.GetCategorias(nombreConjunto).ToList().Count;
             Console.WriteLine(count);
             Assert.IsTrue(count >= 1);
+        }
+
+        [TestMethod]
+        public void TestGetCategoriasConMasDeNPreguntas()
+        {
+            String pNombreConjunto= "OpentDb";
+            int n = 35;
+            int count = ControladorPreguntas.GetCategoriasConMasDeNPreguntas(pNombreConjunto, n).ToList().Count;
+            Console.WriteLine(count);
+            Assert.IsTrue(count >= 1);
+
+        }
+
+        [TestMethod]
+        public void TestCantidadDePreguntasParaCategoria()
+        {
+            String pIdCategoria = "History";
+            int count = ControladorPreguntas.CantidadDePreguntasParaCategoria(pIdCategoria);
+            Assert.IsNotNull(count);
+            Console.WriteLine(count);
+        }
+
+        [TestMethod]
+        public void GetDificultades()
+        {
+            string nombreConjunto = "OpentDb";
+            int count = ControladorPreguntas.GetDificultades(nombreConjunto).ToList().Count;
+            Console.WriteLine(count);
+            Assert.IsTrue(count == 3);
         }
 
 
