a780d96a81257c4fdb1a5125377b66ac749e042d
ezequiel klug <ezeklug@gmail.com>
2021-03-04 22:40:41 -0300

    Cambios en testControladorExamen

--- UnitTests/TestControladorExamen.cs
+++ UnitTests/TestControladorExamen.cs
@@ -12,45 +12,59 @@ namespace UnitTests
     public class TestControladorExamen
     {
         [TestMethod]
-        public void TestInicializarExamen()
+        public void InicializarExamen_ExamenValido_DevuelveExamen()
         {
             var examenDto = ControladorExamen.InicializarExamen("10", "OpentDb", "Science: Computers", "easy");
             Assert.IsTrue(examenDto.ExamenPreguntas.ToList().Count == 10);
+        }
 
+        [TestMethod]
+        public void InicializarExamen_ExamenVacio_DevuelveExcepcionNull()
+        {
             Assert.ThrowsException<ArgumentNullException>(() => ControladorExamen.InicializarExamen("", "", "", ""));
+        }
+
+        [TestMethod]
+        public void InicializarExamen_ExamenConConjuntoInexistente_DevuelveArgumentException()
+        {
             Assert.ThrowsException<ArgumentException>(() => ControladorExamen.InicializarExamen("10", "ConjuntoQueNoExiste", "Science: Computers", "easy"));
+        }
+
+        [TestMethod]
+        public void InicializarExamen_ExamenConCategoriaInexistente_DevuelveArgumentException()
+        {
             Assert.ThrowsException<ArgumentException>(() => ControladorExamen.InicializarExamen("10", "OpentDb", "CategoriaQueNoExiste", "easy"));
         }
 
         [TestMethod]
-        public void TestFuncionalidadExamen()
+        public void IniciarExamen_UsuarioValidoExamenValido_DevuelveExamen()
+        {
+            var examen = ControladorExamen.InicializarExamen("10", "OpentDb", "Science: Computers", "easy");
+            examen = ControladorExamen.IniciarExamen("leo", examen);
+            Assert.IsTrue(examen.CantidadPreguntas == 10);
+        }
+
+        [TestMethod]
+        public void IniciarExamen_UsuarioInexistenteExamenValido_DevuelveUsrNoEncontradoException()
         {
             var examen = ControladorExamen.InicializarExamen("10", "OpentDb", "Science: Computers", "easy");
             Assert.ThrowsException<UsrNoEncontradoException>(() => ControladorExamen.IniciarExamen("usuarioQueNoExiste", examen));
-            examen = ControladorExamen.IniciarExamen("leo", examen);
-
-            TestGuardarPreguntas(examen);
         }
 
-
-        public void TestGuardarPreguntas(ExamenDTO pExamen)
+        [TestMethod]
+        public void GuardarRespuestaFinalizarExamen_ExamenValidoPreguntaValidaRespuestaValida_DevuelveExamen()
         {
-            var preguntas = ControladorExamen.GetPreguntasDeExamen(pExamen.Id);
+            var examen = ControladorExamen.InicializarExamen("10", "OpentDb", "Science: Computers", "easy");
+            var preguntas = ControladorExamen.GetPreguntasDeExamen(examen.Id);
             var idRespuestasElegidas = new List<int>();
-
-            Assert.ThrowsException<ArgumentException>(() => ControladorExamen.GuardarRespuesta(pExamen, preguntas.First(), 0));
-
-            //Guardar las respuestas y finalizar examen
             foreach (var p in preguntas)
             {
                 var idRespuesta = p.Respuestas.First().Id;
                 idRespuestasElegidas.Add(idRespuesta);
-                ControladorExamen.GuardarRespuesta(pExamen, p, idRespuesta);
+                ControladorExamen.GuardarRespuesta(examen, p, idRespuesta);
             }
-            ControladorExamen.FinalizarExamen(pExamen);
-
-            /// Chequea que el examen se haya guardado correctamente el DB
-            var examenGuardadoDTO = ControladorAdministrativo.GetExamenes().First(e => e.Id == pExamen.Id);
+            ControladorExamen.FinalizarExamen(examen);
+            var examenGuardadoDTO = ControladorAdministrativo.GetExamenes().First(e => e.Id == examen.Id);
             foreach (var ep in examenGuardadoDTO.ExamenPreguntas)
             {
                 Assert.IsTrue(idRespuestasElegidas.Contains(ep.RespuestaElegidaId));
@@ -58,6 +72,14 @@ namespace UnitTests
             Assert.IsTrue(examenGuardadoDTO.ExamenPreguntas.ToList().Count == preguntas.Count());
         }
 
+        [TestMethod]
+        public void GuardarRespuesta_ExamenValidoPreguntaValidaRespuestaInvalida_DevuelveArgumentException()
+        {
+            var examen = ControladorExamen.InicializarExamen("10", "OpentDb", "Science: Computers", "easy");
+            var preguntas = ControladorExamen.GetPreguntasDeExamen(examen.Id);
+            var idRespuestasElegidas = new List<int>();
 
+            Assert.ThrowsException<ArgumentException>(() => ControladorExamen.GuardarRespuesta(examen, preguntas.First(), 0));
+        }
     }
 }
